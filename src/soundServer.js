c7e17cdb202f1ebf55ad80e9a6ae5d9ee4bf6837
shlkllpr <sohil.kollipara@gmail.com>
2020-12-05 18:03:02 -0500

    Moving Server to soundserver.js

---
+++ src/soundServer.js
@@ -0,0 +1,159 @@
+<<<<<<< HEAD
+const http = require('http');
+const static = require('node-static');
+const file = new static.Server('./');
+const crypto = require('crypto');
+const server = http.createServer((req, res) => {
+  req.addListener('end', () => file.serve(req, res)).resume();
+});
+
+server.on('upgrade', function (req, socket) {
+  console.log('upgrade');
+  if (req.headers['upgrade'] !== 'websocket') {
+    socket.end('HTTP/1.1 400 Bad Request');
+    return;
+  }
+  // Read the websocket key provided by the client:
+  const acceptKey = req.headers['sec-websocket-key'];
+  // Generate the response value to use in the response:
+  const hash = generateAcceptValue(acceptKey);
+  // Write the HTTP response into an array of response lines:
+  const responseHeaders = [ 'HTTP/1.1 101 Web Socket Protocol Handshake', 'Upgrade: WebSocket', 'Connection: Upgrade', `Sec-WebSocket-Accept: ${hash}` ];
+  // Write the response back to the client socket, being sure to append two
+  // additional newlines so that the browser recognises the end of the response
+  // header and doesn't continue to wait for more header data:
+  socket.write(responseHeaders.join('\r\n') + '\r\n\r\n');
+});
+ // Don't forget the hashing function described earlier:
+function generateAcceptValue (acceptKey) {
+  return crypto
+  .createHash('sha1')
+  .update(acceptKey + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11', 'binary')
+  .digest('base64');
+}
+
+server.on('data', buffer => {
+  console.log("recieved");
+  const message = parseMessage(buffer);
+  if (message) {
+  // For our convenience, so we can see what the client sent
+  console.log(message);
+  // We'll just send a hardcoded message in this example
+  server.write(constructReply({ message: 'Hello from the server!' }));
+  } else if (message === null) {
+      console.log('WebSocket connection closed by the client.');
+  }
+});
+function constructReply (data) {
+  // Convert the data to JSON and copy it into a buffer
+  const json = JSON.stringify(data)
+  const jsonByteLength = Buffer.byteLength(json);
+  // Note: we're not supporting > 65535 byte payloads at this stage
+  const lengthByteCount = jsonByteLength < 126 ? 0 : 2;
+  const payloadLength = lengthByteCount === 0 ? jsonByteLength : 126;
+  const buffer = Buffer.alloc(2 + lengthByteCount + jsonByteLength);
+  // Write out the first byte, using opcode `1` to indicate that the message
+  // payload contains text data
+  buffer.writeUInt8(0b10000001, 0);
+  buffer.writeUInt8(payloadLength, 1);
+  // Write the length of the JSON payload to the second byte
+  let payloadOffset = 2;
+  if (lengthByteCount > 0) {
+    buffer.writeUInt16BE(jsonByteLength, 2); payloadOffset += lengthByteCount;
+  }
+  // Write the JSON data to the data buffer
+  buffer.write(json, payloadOffset);
+  return buffer;
+}
+function parseMessage (buffer) {
+  let maskingKey;
+  if (isMasked) {
+    maskingKey = buffer.readUInt32BE(currentOffset);
+    currentOffset += 4;
+  }
+  // …all of the above, then:
+  // Allocate somewhere to store the final message data
+const data = Buffer.alloc(payloadLength);
+// Only unmask the data if the masking bit was set to 1
+if (isMasked) {
+  // Loop through the source buffer one byte at a time, keeping track of which
+  // byte in the masking key to use in the next XOR calculation
+  for (let i = 0, j = 0; i < payloadLength; ++i, j = i % 4) {
+  // Extract the correct byte mask from the masking key
+    const shift = j = 3 ? 0 : (3 - j) << 3;
+    const mask = (shift == 0 ? maskingKey : (maskingKey >>> shift)) & 0xFF;
+    // Read a byte from the source buffer
+    const source = buffer.readUInt8(currentOffset++);
+    // XOR the source byte and write the result to the data
+    buffer.data.writeUInt8(mask ^ source, i);
+  }
+ } else {
+  // Not masked - we can just read the data as-is
+  buffer.copy(data, 0, currentOffset++);
+}
+  const json = data.toString('utf8'); return JSON.parse(json);
+}
+
+
+const port = 3210;
+server.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+=======
+const express = require('express')
+const cors = require('cors')
+
+const port = 5000
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+async function readPicture(base64string) {
+    const vision = require('@google-cloud/vision');
+    const request = {
+        image: {
+            content: base64string
+        },
+    };
+
+    const client = new vision.ImageAnnotatorClient();
+    const [result1] = await client.textDetection(request);
+    const detections = result1.textAnnotations;
+    let words1 = [];
+    detections.forEach(text => words1.push(text.description));
+    words1.shift()
+    console.log(words1);
+    // const [result2] = await client.documentTextDetection(request);
+    // console.log(result2);
+    // const fullTextAnnotation = result.fullTextAnnotaion;
+    // let words2 = [];
+    // fullTextAnnotation.pages.forEach(page => {
+    //     page.blocks.forEach(block => {
+    //       //console.log(`Block confidence: ${block.confidence}`);
+    //       block.paragraphs.forEach(paragraph => {
+    //         //console.log(`Paragraph confidence: ${paragraph.confidence}`);
+    //         paragraph.words.forEach(word => {
+    //           const wordText = word.symbols.map(s => s.text).join('');
+    //           words2.push(wordText);
+    //           //console.log(`Word text: ${wordText}`);
+    //           //console.log(`Word confidence: ${word.confidence}`);
+    //         //   word.symbols.forEach(symbol => {
+    //         //     console.log(`Symbol text: ${symbol.text}`);
+    //         //     console.log(`Symbol confidence: ${symbol.confidence}`);
+    //         //   });
+    //         });
+    //       });
+    //     });
+    //   });
+    return words1;
+}
+
+app.post('/readPic', async (req, res) => {
+    const png = req.body.png;
+    const words = readPicture(png);
+    res.send(words);
+})
+
+app.listen(port, () => {
+    console.log('listening')
+})
+>>>>>>> 18a2a1c2f9ac9446b096a09c2a2e637257b822ea
