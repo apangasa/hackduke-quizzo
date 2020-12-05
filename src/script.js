37fbdfa5bb9d4d986a38befad687b58429ec20ab
Nitin Ramadoss <nitinramadoss@gmail.com>
2020-12-05 13:54:54 -0500

    Jeopardy Board with hide and update functions

--- src/script.js
+++ src/script.js
@@ -24,3 +24,28 @@ function snapShot(webcam, canvas) {
     console.log(png)
 }
 
+
+function displayBoard(show) {
+  var text = document.querySelector("a-text")
+  var plane = document.querySelector("a-plane")
+  var cylinder1 = document.getElementById("cylinder1")
+  var cylinder2 = document.getElementById("cylinder2")
+  
+  if (show) {
+    text.setAttribute("visible", true);
+    plane.setAttribute("visible", true);
+    cylinder1.setAttribute("visible", true)
+    cylinder2.setAttribute("visible", true)  
+  }  else {
+    text.setAttribute("visible", false);
+    plane.setAttribute("visible", false);
+    cylinder1.setAttribute("visible", false)
+    cylinder2.setAttribute("visible", false)  
+  }
+}
+
+function updateBoard(message){
+  var text = document.querySelector("a-text")   
+  text.setAttribute("value", message)
+}
+
