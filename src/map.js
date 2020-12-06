ded6f0f5770ed8b7d5188f7164b142b480476b6b
Nitin Ramadoss <nitinramadoss@gmail.com>
2020-12-06 01:02:42 -0500

    Adds question interfacea,  additional functions for board updates, and OCR integration

--- src/map.js
+++ src/map.js
@@ -1,40 +1,70 @@
-class QuestionMap {
+class Questions {
 	constructor() {
     	let qMap = new Map()
-    	qMap['neurons'] = [
+    	qMap['acids'] = [
             {
-                "q": "Is a neuron a cell?", 
-                "a": "Yes", 
+                "q": "Acids dissociate to produce?", 
+                "a": "Hydrogen ions", 
+                "score": 500
+            },
+            {
+                "q": "Is HCl and acid?", 
+                "a": "yes", 
                 "score": 100
             },
             {
-                "q": "How many neurons in the brain?", 
-                "a": "trillions", 
-                "score": 500
+                "q": "Is NaOH an acid?", 
+                "a": "yes", 
+                "score": 200
+            },
+            {
+                "q": "What pH is neutral?", 
+                "a": "7", 
+                "score": 300
+            },
+            {
+                "q": "Is the stomach acidic or basic?", 
+                "a": "acidic", 
+                "score": 400
             }
         ]
-        qMap['molecules'] = [
+        qMap['digestion'] = [
             {
-                "q": "Does water contain oxygen?", 
-                "a": "Yes", 
-                "score": 100
+                "q": "Digestion is a _ process", 
+                "a": "mechanical", 
+                "score": 300
             },
             {
-                "q": "Number of molecules in face", 
-                "a": "trillions", 
+                "q": "Promotes chemical digestion?", 
+                "a": "enzymes", 
+                "score": 200
+            },
+            {
+                "q": "The stomach enzyme is?", 
+                "a": "pepsin", 
                 "score": 500
+            },
+            {
+                "q": "Process by which nutrients are taken in?", 
+                "a": "absorption", 
+                "score": 400
+            },
+            {
+                "q": "First stage of food processing?", 
+                "a": "ingestion", 
+                "score": 100
             }
         ]
 
         let topicSet = new Set()
-        topicSet.add("neurons")
-        topicSet.add("molecules")
+        topicSet.add("acids")
+        topicSet.add("digestion")
 
         this.qMap = qMap
         this.topicSet = topicSet
     }
     
-	getQuestions(topic) {
+	  getQuestions(topic) {
     	return this.qMap[topic];
     }
 
--- src/script.js
+++ src/script.js
@@ -1,77 +1,86 @@
 window.onload = function() {
-    initiateWebCam();
-    displayBoard(true);
+  displayBoard(true);
+  runOCR();
 };
 
-
-function initiateWebCam() {
-    let videoElement = document.getElementById('video');
-    let canvas = document.getElementById('canvas');
-    const webcam = new Webcam(videoElement, 'user', canvas);
-
-    webcam.start()
-        .then(result =>{
-        console.log("Video On");
-    })
-        .catch(err => {
-        console.log(err);
-    });
-
-    setInterval(function(){snapShot(webcam, canvas)}, 6000);
-} 
-
-function snapShot(webcam, canvas) {
-    let png = webcam.snap();
-    png = png.split(',')[1]
-    
-    const data = { png: png };
-    const readPictureURL = 'https://31590508e157.ngrok.io/readPic';
-    fetch(readPictureURL, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-    })
-    .then(response => response.json())
-    .then(words => {
-    console.log('Success:', words);
-    })
-    .catch((error) => {
-    console.error('Error:', error);
-    });
+function runOCR() {
+  var b64 = b64_img1;
+  const data = { png: b64 };
+  const readPictureURL = 'https://us-central1-quizzo-297716.cloudfunctions.net/readPic';
+  fetch(readPictureURL, {
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+  console.log('Success:', data);
+  analyzeData(data);
+  })
+  .catch((error) => {
+  console.error('Error:', error);
+  });
 }
 
+function analyzeData(data) {
+let store = new Questions()
+let qObjs = []
+let validTopics = ""
+
+data.words.forEach(function(word) {
+  if (store.isTopic(word) && !qObjs.includes(word)) {
+    validTopics +=  " " + word
+    qObjs.push(word)   
+  }  
+})
+informUser('Topics found: ' + validTopics)
+
+updateBoard(store.qMap[qObjs[0]][0].q)
+}
 
 function displayBoard(show) {
-  var text = document.querySelector("a-text")
-  var plane = document.querySelector("a-plane")
-  var cylinder1 = document.getElementById("cylinder1")
-  var cylinder2 = document.getElementById("cylinder2")
-  
-  if (show) {
-    text.setAttribute("visible", true);
-    plane.setAttribute("visible", true);
-    cylinder1.setAttribute("visible", true)
-    cylinder2.setAttribute("visible", true)  
-  }  else {
-    text.setAttribute("visible", false);
-    plane.setAttribute("visible", false);
-    cylinder1.setAttribute("visible", false)
-    cylinder2.setAttribute("visible", false)  
-  }
+var text = document.querySelector("a-text")
+var plane = document.querySelector("a-plane")
+var cylinder1 = document.getElementById("cylinder1")
+var cylinder2 = document.getElementById("cylinder2")
+
+if (show) {
+  text.setAttribute("visible", true);
+  plane.setAttribute("visible", true);
+  cylinder1.setAttribute("visible", true)
+  cylinder2.setAttribute("visible", true)  
+}  else {
+  text.setAttribute("visible", false);
+  plane.setAttribute("visible", false);
+  cylinder1.setAttribute("visible", false)
+  cylinder2.setAttribute("visible", false)  
+}
+}
+
+function stopARCamera() {
+var arCamera = document.querySelector("a-camera") 
+var scene = document.querySelector("a-scene") 
+
+scene.removeChild(arCamera)
 }
 
 function updateBoard(message){
-  var text = document.querySelector("a-text")   
-  text.setAttribute("value", message)
+var text = document.querySelector("a-text")   
+text.setAttribute("value", message)
 }
 
 function informUser(message) {
-    var modal = document.getElementById("modalElement");
-    var modalText = document.getElementById("messageBox");
-    
-    modal.style.display = "block";
-    modalText.innerHTML = message
+  var modal = document.getElementById("modalElement");
+  var modalText = document.getElementById("messageBox");
+  
+  modal.style.display = "block";
+  modalText.innerHTML = message
+  
+  setTimeout(function(modal){modal.style.display = "none"}, 2000, modal);
 }
 
+function stopCamera(webcam) {
+  webcam.stop();
+}
